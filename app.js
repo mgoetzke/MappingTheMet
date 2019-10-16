609cf68cb63b451422bfe0756d6c9e198f52dd56
mgoetzke <mgoetzke@gmail.com>
2019-10-15 20:44:46 -0400

    Rework all country api call so it actually works

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require("path");
 const fetch = require("node-fetch");
 const PORT = process.env.PORT || 8080; // process.env accesses heroku's environment variables
-
+const Axios = require('axios');
 app.use(express.static("public"));
 
 app.get("/", (request, res) => {
@@ -27,9 +27,7 @@ app.get("/objects/:location", (request, response) => {
 });
 
 // create route to get all objects by all
-app.get("/objects/all", (request, response) => {
-  console.log("hey");
-  debugger;
+app.get("/objects/", (request, response) => {
   // make api call using fetch
   let countries = [
     "Afghanistan",
@@ -228,257 +226,35 @@ app.get("/objects/all", (request, response) => {
     "Uganda",
     "Ukraine",
     "United Arab Emirates",
-    "United Kingdom"
-    // "Uruguay",
-    // "Uzbekistan",
-    // "Venezuela",
-    // "Vietnam",
-    // "Virgin Islands (US)",
-    // "Yemen",
-    // "Zambia",
-    // "Zimbabwe"
+    "United Kingdom",
+    "Uruguay",
+    "Uzbekistan",
+    "Venezuela",
+    "Vietnam",
+    "Virgin Islands (US)",
+    "Yemen",
+    "Zambia",
+    "Zimbabwe"
   ];
+  console.log("hi");
   let promises = countries.map(country => {
-    debugger;
-    console.log({ country });
     return fetch(
       `https://collectionapi.metmuseum.org/public/collection/v1/search?geoLocation=${country}&q=""`
-    );
+    ).then(response => {
+      return response.text();
+    })
+    .then(body => {
+      let results = JSON.parse(body);
+      return {country: country, total: results.total};
+    });
   });
-  // Promise.all(promises)
-  //   .then(response => Promise.all(response.map(r => r.text())))
-  //   .then(bodies => {
-  //     Promise.all(body.map(console.log(results)));
-  //   });
-  Promise.all(promises).then(response => {
-    debugger;
-    console.log("test");
-    Promise.all(response.map(res => res.text()));
-    console.log("ughughughugh");
-    debugger;
+  Promise.all(promises).then(data => {
+    console.log("First handler", data);
   });
+
 });
 
-// create route to get all objects by culture
-// app.get("/objects/countries", (request, response) => {
-//   // make api call using fetch
-//    let countries = [
-//     "Afghanistan",
-//     "Albania",
-//     "Algeria",
-//     "Andorra",
-//     "Angola",
-//     "Anguilla",
-//     "Antigua &amp; Barbuda",
-//     "Argentina",
-//     "Armenia",
-//     "Aruba",
-//     "Australia",
-//     "Austria",
-//     "Azerbaijan",
-//     "Bahamas",
-//     "Bahrain",
-//     "Bangladesh",
-//     "Barbados",
-//     "Belarus",
-//     "Belgium",
-//     "Belize",
-//     "Benin",
-//     "Bermuda",
-//     "Bhutan",
-//     "Bolivia",
-//     "Bosnia &amp; Herzegovina",
-//     "Botswana",
-//     "Brazil",
-//     "British Virgin Islands",
-//     "Brunei",
-//     "Bulgaria",
-//     "Burkina Faso",
-//     "Burundi",
-//     "Cambodia",
-//     "Cameroon",
-//     "Cape Verde",
-//     "Cayman Islands",
-//     "Chad",
-//     "Chile",
-//     "China",
-//     "Colombia",
-//     "Congo",
-//     "Cook Islands",
-//     "Costa Rica",
-//     "Cote D Ivoire",
-//     "Croatia",
-//     "Cruise Ship",
-//     "Cuba",
-//     "Cyprus",
-//     "Czech Republic",
-//     "Denmark",
-//     "Djibouti",
-//     "Dominica",
-//     "Dominican Republic",
-//     "Ecuador",
-//     "Egypt",
-//     "El Salvador",
-//     "Equatorial Guinea",
-//     "Estonia",
-//     "Ethiopia",
-//     "Falkland Islands",
-//     "Faroe Islands",
-//     "Fiji",
-//     "Finland",
-//     "France",
-//     "French Polynesia",
-//     "French West Indies",
-//     "Gabon",
-//     "Gambia",
-//     "Georgia",
-//     "Germany",
-//     "Ghana",
-//     "Gibraltar",
-//     "Greece",
-//     "Greenland",
-//     "Grenada",
-//     "Guam",
-//     "Guatemala",
-//     "Guernsey",
-//     "Guinea",
-//     "Guinea Bissau",
-//     "Guyana",
-//     "Haiti",
-//     "Honduras",
-//     "Hong Kong",
-//     "Hungary",
-//     "Iceland",
-//     "India",
-//     "Indonesia",
-//     "Iran",
-//     "Iraq",
-//     "Ireland",
-//     "Isle of Man",
-//     "Israel",
-//     "Italy",
-//     "Jamaica",
-//     "Japan",
-//     "Jersey",
-//     "Jordan",
-//     "Kazakhstan",
-//     "Kenya",
-//     "Kuwait",
-//     "Kyrgyz Republic",
-//     "Laos",
-//     "Latvia",
-//     "Lebanon",
-//     "Lesotho",
-//     "Liberia",
-//     "Libya",
-//     "Liechtenstein",
-//     "Lithuania",
-//     "Luxembourg",
-//     "Macau",
-//     "Macedonia",
-//     "Madagascar",
-//     "Malawi",
-//     "Malaysia",
-//     "Maldives",
-//     "Mali",
-//     "Malta",
-//     "Mauritania",
-//     "Mauritius",
-//     "Mexico",
-//     "Moldova",
-//     "Monaco",
-//     "Mongolia",
-//     "Montenegro",
-//     "Montserrat",
-//     "Morocco",
-//     "Mozambique",
-//     "Namibia",
-//     "Nepal",
-//     "Netherlands",
-//     "Netherlands Antilles",
-//     "New Caledonia",
-//     "New Zealand",
-//     "Nicaragua",
-//     "Niger",
-//     "Nigeria",
-//     "Norway",
-//     "Oman",
-//     "Pakistan",
-//     "Palestine",
-//     "Panama",
-//     "Papua New Guinea",
-//     "Paraguay",
-//     "Peru",
-//     "Philippines",
-//     "Poland",
-//     "Portugal",
-//     "Puerto Rico",
-//     "Qatar",
-//     "Reunion",
-//     "Romania",
-//     "Russia",
-//     "Rwanda",
-//     "Saint Pierre &amp; Miquelon",
-//     "Samoa",
-//     "San Marino",
-//     "Satellite",
-//     "Saudi Arabia",
-//     "Senegal",
-//     "Serbia",
-//     "Seychelles",
-//     "Sierra Leone",
-//     "Singapore",
-//     "Slovakia",
-//     "Slovenia",
-//     "South Africa",
-//     "South Korea",
-//     "Spain",
-//     "Sri Lanka",
-//     "St Kitts &amp; Nevis",
-//     "St Lucia",
-//     "St Vincent",
-//     "St. Lucia",
-//     "Sudan",
-//     "Suriname",
-//     "Swaziland",
-//     "Sweden",
-//     "Switzerland",
-//     "Syria",
-//     "Taiwan",
-//     "Tajikistan",
-//     "Tanzania",
-//     "Thailand",
-//     "Timor L'Este",
-//     "Togo",
-//     "Tonga",
-//     "Trinidad &amp; Tobago",
-//     "Tunisia",
-//     "Turkey",
-//     "Turkmenistan",
-//     "Turks &amp; Caicos",
-//     "Uganda",
-//     "Ukraine",
-//     "United Arab Emirates",
-//     "United Kingdom",
-//     "Uruguay",
-//     "Uzbekistan",
-//     "Venezuela",
-//     "Vietnam",
-//     "Virgin Islands (US)",
-//     "Yemen",
-//     "Zambia",
-//     "Zimbabwe"
-//   ];
-//   let promises = countries.map(country => {
-//     debugger;
-//     console.log({ country });
-//     return fetch(
-//       `https://collectionapi.metmuseum.org/public/collection/v1/search?geoLocation=${country}&q=""`
-//     );
-//   });
 
-//   return await Promise.all(promises);
-// });
 
 app.listen(PORT, () => {
   console.log(__dirname);
