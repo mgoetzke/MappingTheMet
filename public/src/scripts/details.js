741b03056e9e65c8e50a79f013cec561c1f824ee
mgoetzke <mgoetzke@gmail.com>
2019-10-30 20:48:09 -0400

    Arrange elements in column

--- public/src/scripts/details.js
+++ public/src/scripts/details.js
@@ -98,7 +98,7 @@ export function renderDetails(prompt) {
           ]
         },
         options: {
-          responsive: false,
+          responsive: true,
           scales: {
             yAxes: [
               {
